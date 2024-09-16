2399664939db51611062bbde00c3d0e8a808d9fb
Raphael Saldanha <rfsaldanha@gmail.com>
2024-09-16 08:47:07 +0200

    Import data from full CSV database

--- 01_prepare_database.R
+++ 01_prepare_database.R
@@ -1,31 +1,18 @@
 library(tidyverse)
 library(duckdb)
 
-# Read data
-aih_teste <- read_delim(
-  file = "../test_AIH_emergencias.csv",
-  delim = ";", na = c("", "NA")
+# Create database connection
+con <- dbConnect(duckdb(), dbdir = "aih.duckdb", read_only = FALSE)
+
+# Import AIH csv
+duckdb_read_csv(
+  conn = con, 
+  name = "aih", 
+  files = "../tb_aih_emergencias.csv",
+  delim = ";", 
+  header = TRUE, 
+  na.strings = "NA"
 )
-
-# Create database
-con <- dbConnect(duckdb(), dbdir = "aih_teste.duckdb", read_only = FALSE)
-
-# Populate with AIH data frame
-# Vanderlei comments: 
-# Foi considerado os CID-10 do capítulo XX, 
-# que reúne outras causas externas de lesões acidentais
-# (W00 a X59 e X-30 a X-39), 
-# o capítulo XIX, que reúne as lesões, envenenamento e algumas 
-# outras consequências de causas externas (S00 a T98) 
-# e o capítulo I, que reúne algumas doenças infecciosas e parasitárias 
-# (A27).
-
-dbWriteTable(con, "aih", aih_teste)
-
-
-# Remove data frame
-rm(aih_teste)
-gc()
 
 # Table alias
 aih_tbl <- tbl(con, "aih")
