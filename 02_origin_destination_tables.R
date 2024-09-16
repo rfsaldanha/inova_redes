74e011f8bbc3d15d668c4abf949e7a7730e47611
Raphael Saldanha <rfsaldanha@gmail.com>
2024-09-16 09:43:46 +0200

    Aggregate dates to month

--- 02_origin_destination_tables.R
+++ 02_origin_destination_tables.R
@@ -16,16 +16,30 @@ aih_tbl |>
 
 # All procedures, by date of admission
 od_all_proc <- aih_tbl |>
+  # Dates to month
+  mutate(
+    dt_inter = as_date(dt_inter),
+    year = year(dt_inter),
+    month = month(dt_inter)
+  ) |>
   # Compute frequency
-  group_by(dt_inter, munic_res, munic__mov) |>
+  group_by(year, month, munic_res, munic__mov) |>
   summarise(freq = n()) |>
   ungroup() |>
   # Retrieve from database to memory
   collect() |>
   # Rename variable
   rename(munic_mov = munic__mov) |>
-  # Change to date type
-  mutate(dt_inter = as_date(dt_inter))
+  # Recreate date variable
+  mutate(dt_inter = as_date(paste0(year, "-", month, "-01"))) |>
+  # Select and relocate variables
+  select(-year, -month) |>
+  relocate(dt_inter) |>
+  # Filter date
+  filter(dt_inter >= as_date("2013-01-01"))
 
 # Export
 write_rds(x = od_all_proc, file = "od_all_proc.rds")
+
+# Disconnect database
+dbDisconnect(con)
