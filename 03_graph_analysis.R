125542084bc3b87c95501db1d8d5b9b675954b68
Raphael Saldanha <rfsaldanha@gmail.com>
2024-09-16 10:46:19 +0200

    igraph usage

--- 03_graph_analysis.R
+++ 03_graph_analysis.R
@@ -19,3 +19,18 @@ out_all_proc |>
   filter(munic_res == 110001) |>
   ggplot(aes(x = dt_inter, y = out_freq)) +
   geom_line(stat = "identity")
+
+# Graph, all time
+g_all <-  od_all_proc |>
+  # Remove loops
+  filter(!(munic_res == munic_mov)) |>
+  # Aggregate all dates
+  group_by(munic_res, munic_mov) |>
+  summarise(freq = sum(freq)) |>
+  ungroup() |>
+  # Rename fields
+  rename(from = munic_res, to = munic_mov, weight = freq) |>
+  # Graph structure
+  graph_from_data_frame(directed = TRUE)
+
+degree(g_all, mode = "out")
